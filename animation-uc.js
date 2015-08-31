d9ecd53fbea9c09c799092b9177105660dc629b2
ssohjiro@gmail.com <ssohjiro@gmail.com>
2015-09-01 02:49:57 +0900

    update to 0.7 after a long interval

--- animation-uc.js
+++ animation-uc.js
@@ -44,7 +44,7 @@
 	// Finally, as a browser global.
 	} else {
 		root.logger = root.log;	// in case of loglevel
-		root.aniUC = factory(root, {}, root.jQuery, root.rafUC.raf, root.rafUC.caf, root.ease, root._, root.logger );
+		root.aniUC = factory(root, {}, root.rafUC.raf, root.rafUC.caf, root.ease, root._, root.logger );
 	}
 	
 }( function( root, aniUC, raf, caf, ease, _, logger ) {
@@ -65,6 +65,29 @@
 	var defaultOptions = {
 		duration: 500
 	};
+	
+	function getTransform(el) {
+		var transform = window.getComputedStyle(el, null).getPropertyValue('transform');
+		//var results = transform.match(/matrix(?:(3d)\(-{0,1}\d+(?:, -{0,1}\d+)*(?:, (-{0,1}\d+))(?:, (-{0,1}\d+))(?:, (-{0,1}\d+)), -{0,1}\d+\)|\(-{0,1}\d+(?:, -{0,1}\d+)*(?:, (-{0,1}\d+))(?:, (-{0,1}\d+))\))/);
+		//var results = transform.match(/matrix(?:(3d)\(\d+(?:, \d+)*(?:, (\d+))(?:, (\d+))(?:, (\d+)), \d+\)|\(\d+(?:, \d+)*(?:, (\d+))(?:, (\d+))\))/)
+
+
+		var results = transform.match(/matrix(?:(3d)\(-{0,1}\d+\.?\d*(?:, -{0,1}\d+\.?\d*)*(?:, (-{0,1}\d+\.?\d*))(?:, (-{0,1}\d+\.?\d*))(?:, (-{0,1}\d+\.?\d*)), -{0,1}\d+\.?\d*\)|\(-{0,1}\d+\.?\d*(?:, -{0,1}\d+\.?\d*)*(?:, (-{0,1}\d+\.?\d*))(?:, (-{0,1}\d+\.?\d*))\))/);
+
+
+		if(!results) return [0, 0, 0];
+		if(results[1] == '3d') return results.slice(2,5).map( Number );
+
+		results.push(0);
+		return results.slice(5, 8).map(Number); // returns the [X,Y,Z,1] values
+	}
+	
+
+	aniUC.getTransform = function(el ) {
+		return getTransform( el );
+	};
+
+	var animatingList = {};
 
 	aniUC.tween = function( el, endPos, options ) {
 
@@ -72,22 +95,50 @@
 
 		options = _.extend({}, defaultOptions, options );
 
+		var id = _.uniqueId('animating');
+
+		animatingList[ id ] = [{ el: el }];
 		var startPos = {};
 
 		_.each( endPos, function( val, key ) {
+
 			if( key === 'scrollTop' ) {
+
 				startPos.scrollTop = el.scrollTop;
+
 			} else if(['x','y','z'].indexOf( key ) > -1 ) {
 				// transform's translate3d style
+
+				if( startPos.x === undefined ) {
+					var translate3d = getTransform( el );
+					startPos.x = translate3d[0] || 0;
+					startPos.y = translate3d[1] || 0;
+					startPos.z = translate3d[2] || 0;
+					startPos.transform = 'translate3d(' + startPos.x + 'px, '+ startPos.y+'px,' + startPos.z+ 'px)';
+					endPos.transform = 'translate3d(' + (endPos.x||0) + 'px, '+ (endPos.y||0)+'px,' + (endPos.z||0)+ 'px)';
+				}
+
 			} else if( cssAttrs.indexOf( key ) > -1 ) {
+
 				var computedStyle = getComputedStyle( el );
-				startPos[ key ] = Number( computedStyle[ key ].replace('px','') || 0 );
+				var position = computedStyle.position;
+
+				var tmpStartPos = computedStyle[ key ].replace('px','');
+				if( key === 'top' && tmpStartPos === 'auto' ) {
+					if( position === 'absolute' ) tmpStartPos = el.offsetTop;
+					else if( position === 'relative' ) tmpStartPos = 0;
+				}
+
+				startPos[ key ] = Number( tmpStartPos );
 
 				if( typeof endPos[ key ] === 'string' ) {
 					endPos[key] = Number( endPos[ key ].replace('px','') );
 				}
 			}
 		});
+
+		console.log( startPos, endPos );
+
 
 		var startTime = Date.now();
 		var stop = false;
@@ -98,7 +149,7 @@
 			var cur = {};
 			var now = Date.now();
 
-			if( now - startTime >= options.duration ) {
+			if( now - startTime >= options.duration || animatingList[ id ].stop ) {
 				stop = true;
 				complete = true;
 			}
@@ -110,6 +161,7 @@
 						cur.scrollTop = endPos.scrollTop;
 					} else if(['x','y','z'].indexOf( key ) > -1 ) {
 						// transform's translate3d style
+						cur.transform = endPos.transform;
 					} else if( cssAttrs.indexOf( key ) > -1 ) {
 						cur[ key ] = endPos[ key ];
 					}
@@ -120,27 +172,34 @@
 			} else {
 				var p = ( now - startTime ) / options.duration;
 				var r = ease.outQuad( p );
+				var x,y,z;
 
 				_.each( startPos, function( val, key ) {
-					if( key === 'scrollTop' ) {
-						cur.scrollTop = val + ( endPos.scrollTop - val )*r;
-					} else if(['x','y','z'].indexOf( key ) > -1 ) {
-						// transform's translate3d style
-					} else if( cssAttrs.indexOf( key ) > -1 ) {
-						cur[ key ] = val + ( endPos[ key ] - val ) * r;
-					}
+					cur[ key ] = val + ( endPos[ key ] - val ) * r;
 				});
+
+				cur.x = cur.x || 0;
+				cur.y = cur.y || 0;
+				cur.z = cur.z || 0;
+				cur.transform = 'translate3d(' + cur.x + 'px, '+ cur.y+'px,' + cur.z+ 'px)';
+				//console.log( cur.transform );
 
 				//cur.scrollTop = startPos.scrollTop + ( endPos.scrollTop - startPos.scrollTop ) * r;
 			}
 
+
+			// 여기서 값 셋팅
 			_.each( startPos, function( val, key ) {
 				if( key === 'scrollTop' ) {
 					el.scrollTop = cur.scrollTop;
-				} else if(['x','y','z'].indexOf( key ) > -1 ) {
-					// transform's translate3d style
+				} else if( key === 'transform' ) {
+
+					el.style[ key ] = cur[ key ];
+
 				} else if( cssAttrs.indexOf( key ) > -1 ) {
+					
 					el.style[ key ] = cur[ key ] + 'px';
+					logger.debug( el.style[ key ] );
 				}
 			});
 			//el.scrollTop = cur.scrollTop;
@@ -165,5 +224,6 @@
 		animate();
 	};
 	
+	window.aniUC = aniUC;
 	return aniUC;
 }));
--- examples/example.js
+++ examples/example.js
@@ -1,2 +1,18 @@
-var aniUC = require('./core');
-aniUC.tween( document.getElementById('box'), { scrollTop: 400 });
+
+var aniUC = require('../animation-uc');
+
+aniUC.tween( document.getElementById('box'), { y: 400, x:200 }, {
+	duration: 5000,
+	complete: function() {
+		console.log('ok');
+	}
+});
+
+setTimeout( function() {
+	aniUC.tween( document.getElementById('box'), { y: 0, x:0 }, {
+		duration: 5000,
+		complete: function() {
+			console.log('ok2');
+		}
+	});
+}, 2000 );
